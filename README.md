9aa94dc47983fbcecb5721f90f74149390daaae1
John Morrison <jrgmorrison@gmail.com>
2013-04-05 21:51:03 -0700

    Remap DNS requests for some.host.tld to use a specific ELB in an AWS region

---
+++ README.md
@@ -0,0 +1,82 @@
+This is a little tool that I needed so that I could point my laptop at an AWS
+region running Elastic Load Balancing that was not yet in production, but
+refer to that region by the public HTTPS domain name that it would eventually
+be serving.
+
+## Installing:
+
+* `git clone git://github.com/jrgm/dnshook.git`
+
+* `cd dnshook`
+
+* Copy `config.json-dist` to `config.json` and edit that file to map the
+  public domain name(s) to one (or more) ELB domain names.
+
+* `npm install`
+
+* `./dnshook.js --help`:
+
+    Remap DNS requests for some.host.tld to use a specific ELB in an AWS region
+    
+    Usage: node ./dnshook.js [options]
+    
+    Options:
+      --help           display this usage message                                   
+      --real-resolver  what resolver to use for domains we are not intercepting, or
+                       for when we need to get the real answer  [default: "8.8.8.8"]
+      --port           what port to bind                               [default: 53]
+      --config-file    path/to/config.json                [default: "./config.json"]
+      --logfile        path/to/logging.log                [default: "./logging.log"]
+
+## Running:
+
+* change your DNS configuration to use 127.0.0.1 as the resolver. (See notes
+  below on how to change this from the OSX command line).
+
+* `sudo ./dnshook [options]`. Usually the defaults are what you need.
+
+* Logging will go both to the console, and to the `--logfile`
+
+## How to change the OSX resolver address from the command line:
+
+### List the available interfaces (you probably want the wireless one):
+
+    $ networksetup listallnetworkservices
+      Ethernet
+      Wi-Fi
+      iPhone USB
+
+### List your current dns server(s).
+    
+    $ sudo networksetup -getdnsservers Wi-Fi
+    
+### Set it to where you want, e.g.:
+
+    $ sudo networksetup -setdnsservers Wi-Fi 127.0.0.1
+
+### Set it back to default:
+
+    $ sudo networksetup -setdnsservers Wi-Fi <your original resolver list>
+
+    $ sudo networksetup -setdnsservers Wi-Fi empty
+
+    Note: type the above line exactly (i.e., 'empty' doesn't mean '')
+
+
+## Caveats:
+
+1. I looked into port forwarding port 53 for localhost for osx, but couldn't
+   find something that worked. So for now, this must be run as sudo. If anyone
+   has a solution, let me know.
+
+2. This is a test tool right now, so I don't recommend running it all the
+   time. For now, change the resolver address, start this tool. When done,
+   stop the tool and change your resolver back to the original value. (But I
+   have used this as my resolver for hours at a time).
+
+3. This is not compatible with using DNS addresses via a VPN.
+
+## Notes:
+
+1. The mapping can be an identity map. This repo was pushed to github when
+   this code was remapping github.com to github.com
---
+++ dnshook.js
@@ -0,0 +1,179 @@
+#!/usr/bin/env node
+
+const
+cjson = require('cjson'),
+dns = require('native-dns'),
+fs = require('fs'),
+optimist = require('optimist'),
+winston = require('winston');
+
+var logger;
+
+function configureLogging(options) {
+  var config = {
+    timestamp: function () { return new Date().toISOString(); },
+    filename: options.logfile || './logging.log',
+    handleExceptions: true,
+    exitOnError: false,
+  };
+
+  logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)(config),
+      new (winston.transports.File)(config),
+    ]
+  });
+
+  // monkeypatch winston to accept multiple args like console.log
+  var levels = {};
+  Object.keys(winston.config.syslog.levels).forEach(function(level) {
+    levels[level] = logger[level];
+    logger[level] = function() {
+      var args = Array.prototype.slice.call(arguments, 0);
+      levels[level](args.join(' '));
+    };
+  });
+}
+
+function configure() {
+  function configError(message) {
+    console.log('\nError:', message);
+    argv.showHelp();
+    process.exit(1);
+  }
+
+  const usage = 'Remap DNS requests for some.host.tld to use a specific ELB in an AWS region';
+  const options = {
+    'help': {
+      'describe': 'display this usage message'
+    },
+    'real-resolver': {
+      'describe': ('what resolver to use for domains we are not intercepting, ' +
+                   'or for when we need to get the real answer'),
+      'default': '8.8.8.8' // Google public DNS
+    },
+    'port': {
+      'describe': 'what port to bind',
+      'default': 53
+    },
+    'config-file': {
+      'describe': 'path/to/config.json',
+      'default': './config.json'
+    },
+    'logfile': {
+      'describe': 'path/to/logging.log',
+      'default': './logging.log'
+    },
+  };
+
+  var argv = optimist
+    .usage('\n' + usage + '\n\nUsage: $0 [options]')
+    .options(options).wrap(80);
+  var args = argv.argv;
+
+  if (args.help) {
+    argv.showHelp();
+    process.exit(0);
+  }
+
+  if (args.port < 1024 && process.getuid() !== 0) {
+    console.log('Sorry, this program must be run with sudo, so it can bind to port',
+                args.port);
+    process.exit(0);
+  }
+
+  var config = cjson.load(args['config-file']);
+  args.domains = config.domains;
+
+  if (!args['real-resolver'].match(/(\d+\.){3}\d+/)) {
+    configError(args['real-resolver'] + ' does not appear to be an IP address');
+  }
+
+  configureLogging(args);
+
+  return args;
+}
+
+(function run() {
+  var args = configure();
+  var server = dns.createServer();
+
+  var pickOne = function(list) {
+    return list[parseInt(Math.random() * list.length, 10)];
+  };
+
+  server.on('request', function(request, response) {
+    var interceptedDomain;
+    var domain = request.question[0].name;
+    logger.info('server.request', 'for', domain);
+
+    if (args.domains[domain]) {
+      logger.info('server.request', 'intercepting request for', domain);
+      interceptedDomain = domain;
+      domain = pickOne(args.domains[domain]);
+    }
+
+    logger.info('server.request', 'forwarding request for', domain);
+    var req = dns.Request({
+      question: dns.Question({ name: domain, type: 'A' }),
+      server: {
+        address: args['real-resolver'],
+        port: args.port,
+        type: 'udp'
+      },
+      timeout: 5000,
+    });
+
+    req.on('end', function() {
+        logger.info('request.end', req.question.name);
+    });
+
+    [ 'timeout', 'cancelled' ].forEach(function(evt) {
+      req.on(evt, function() {
+        logger.error('request.' + evt, req.question.name);
+      });
+    });
+
+    req.on('error', function(err) {
+      logger.error('request.error', req.question.name, err.stack);
+    });
+
+    req.on('message', function(err, answer) {
+      if (err) {
+        return logger.error('request.message', 'handling response for',
+                            req.question.name, err.stack);
+      }
+
+      logger.info('request.message', 'handling response for', req.question.name);
+
+      answer.answer.forEach(function(ans) {
+        var name = dns.consts.QTYPE_TO_NAME[ans.type];
+        if (name === 'A' && interceptedDomain) {
+          logger.info('request.message', 'fix intercept', interceptedDomain,
+                      'for', domain, 'ip', ans.address);
+          ans.name = interceptedDomain;
+        }
+        response.answer.push(dns[name](ans));
+      });
+      response.send();
+    });
+    req.send();
+  });
+
+  server.on('error', function(err, msg /*, response */) {
+    logger.error('server.error', err.stack, msg);
+  });
+
+  server.on('socketError', function(err) {
+    logger.error('server.socketError', err.stack);
+  });
+
+  [ 'listening', 'close' ].forEach(function(evt) {
+    server.on(evt, function() {
+      logger.info('server.' + evt);
+    });
+  });
+
+  server.serve(args.port, '127.0.0.1');
+  logger.info('Listening on 127.0.0.1:' + args.port, 'for', Object.keys(args.domains).join(', '));
+})();
